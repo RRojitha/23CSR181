5d600218232ffb9014b4fbe98dba41f51fd059e4
RRojitha <rrojitha77@gmail.com>
2026-05-18 11:33:04 +0530

    App.js updated

--- app.js
+++ app.js
@@ -1,1 +1,31 @@
-console.log("helloooo guyyysss")
+const notifications =
+    require("./data/notifications");
+
+const {
+    getTopNotifications
+} = require("./services/priorityService");
+
+const logger =
+    require("./middleware/logger");
+
+logger("Application Started");
+
+const topNotifications =
+    getTopNotifications(notifications, 10);
+
+console.log("\nTop Notifications:\n");
+
+topNotifications.forEach((notification, index) => {
+
+    console.log(
+        `${index + 1}. ${notification.message}`
+    );
+
+    console.log(
+        `Type: ${notification.type}`
+    );
+
+    console.log("----------------------");
+});
+
+
